0d6da4a2084739fbc7c3c93f2e1757c2f1e9265a
Nick Mcmahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-09-18 12:09:17 +0100

    add try catch

--- src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsApplication.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/HocsDocsApplication.java
@@ -1,12 +1,18 @@
 package uk.gov.digital.ho.hocs.document;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class HocsDocsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HocsDocsApplication.class, args);
+		try {
+			SpringApplication.run(HocsDocsApplication.class, args);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
 	}
 }
