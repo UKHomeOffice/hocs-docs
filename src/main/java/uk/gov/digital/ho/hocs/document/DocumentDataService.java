0355ad383afec5251e4918024cb3b7206ad70202
nwoodHO <natalie.wood@digital.homeoffice.gov.uk>
2019-01-31 16:36:02 +0000

    fix failing test in gradle build

--- src/main/java/uk/gov/digital/ho/hocs/document/DocumentDataService.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/DocumentDataService.java
@@ -3,9 +3,7 @@ package uk.gov.digital.ho.hocs.document;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import uk.gov.digital.ho.hocs.document.aws.S3DocumentService;
-import uk.gov.digital.ho.hocs.document.client.auditclient.AuditClient;
 import uk.gov.digital.ho.hocs.document.dto.camel.S3Document;
 import uk.gov.digital.ho.hocs.document.exception.ApplicationExceptions;
 import uk.gov.digital.ho.hocs.document.model.DocumentData;
