69162b1abee821548e3780bf145a8ed1420de090
nwoodHO <natalie.wood@digital.homeoffice.gov.uk>
2019-02-12 13:46:16 +0000

    add unit tests, remove audit for download and update doc

--- src/main/java/uk/gov/digital/ho/hocs/document/client/auditclient/EventType.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/client/auditclient/EventType.java
@@ -1,10 +1,7 @@
 package uk.gov.digital.ho.hocs.document.client.auditclient;
 
-import org.apache.camel.impl.DefaultDebugger;
-
 public enum EventType {
     DOCUMENT_CREATED,
-    DOCUMENT_UPDATED,
     DOCUMENT_DELETED,
-    DOCUMENT_DOWNLOADED;
+    ;
 }
