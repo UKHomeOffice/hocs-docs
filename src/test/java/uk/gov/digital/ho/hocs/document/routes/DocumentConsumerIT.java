781ec68aee1e417559ea4c5ac598775d6c79a2aa
Paul Rau <52495258+Paulcr-ho@users.noreply.github.com>
2020-05-04 13:11:25 +0100

    HOCS-1698 (#96)
    Removed the ignore from the DocumentConsumerIT test

--- src/test/java/uk/gov/digital/ho/hocs/document/routes/DocumentConsumerIT.java
+++ src/test/java/uk/gov/digital/ho/hocs/document/routes/DocumentConsumerIT.java
@@ -38,7 +38,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @RunWith(CamelSpringBootRunner.class)
 @DisableJmx
-@Ignore
 public class DocumentConsumerIT {
 
     private static boolean setUpIsDone = false;
