8df37c7d922cf59235a5e7da6f1f675bd53be249
Paul Rau <paul.rau1@digital.homeoffice.gov.uk>
2020-06-02 10:50:12 +0100

    HOCS-1751 Due to the environment now containing the namespace, there is no need for a separate deployment script for wcs prod. Removed the wcs prod deployment and fixed a misconfiguration in the bucket name (contained the domain)

--- deploy.sh
+++ deploy.sh
@@ -51,16 +51,8 @@ fi
 
 cd kd
 
-if [[ ${KUBE_NAMESPACE} == "wcs-prod" ]] ; then
-    kd --insecure-skip-tls-verify \
-       --timeout 10m \
-        -f deployment-wcs-prod.yaml \
-        -f service.yaml \
-        -f autoscale.yaml
-else
-    kd --insecure-skip-tls-verify \
-       --timeout 10m \
-        -f deployment.yaml \
-        -f service.yaml \
-        -f autoscale.yaml
-fi
+kd --insecure-skip-tls-verify \
+   --timeout 10m \
+    -f deployment.yaml \
+    -f service.yaml \
+    -f autoscale.yaml
