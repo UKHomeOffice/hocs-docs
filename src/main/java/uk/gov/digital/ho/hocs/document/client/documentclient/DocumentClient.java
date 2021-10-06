f60312b661a3783119dbb8a9ba6fce3625617307
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-10-06 09:22:36 +0100

    Remove the retry behaviour around the producer because it doesn't work on a private method anyway, it uses AOP.

--- src/main/java/uk/gov/digital/ho/hocs/document/client/documentclient/DocumentClient.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/client/documentclient/DocumentClient.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import uk.gov.digital.ho.hocs.document.client.documentclient.dto.ProcessDocumentRequest;
 import uk.gov.digital.ho.hocs.document.exception.ApplicationExceptions;
@@ -37,15 +35,10 @@ public class DocumentClient {
     public void processDocument(UUID documentUUID, String fileLocation, String convertTo) {
         ProcessDocumentRequest request = new ProcessDocumentRequest(documentUUID, fileLocation, convertTo);
         try {
-            sendMessage(objectMapper.writeValueAsString(request));
+            producerTemplate.sendBody(documentQueue, objectMapper.writeValueAsString(request));
             log.info("Processed Document {}", documentUUID, value(EVENT, DOCUMENT_CLIENT_PROCESS_SUCCESS));
         } catch (JsonProcessingException e) {
             throw new ApplicationExceptions.EntityCreationException(String.format("Could not process Document: %s", e.toString()), DOCUMENT_CLIENT_FAILURE);
         }
-    }
-
-    @Retryable(maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.delay}"))
-    private void sendMessage(String message) {
-        producerTemplate.sendBody(documentQueue, message);
     }
 }
