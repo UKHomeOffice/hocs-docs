e2218990d721b1d8792a89dacd5716cc0bc42354
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2021-10-11 13:07:40 +0100

    Add headers to document client

--- src/main/java/uk/gov/digital/ho/hocs/document/client/documentclient/DocumentClient.java
+++ src/main/java/uk/gov/digital/ho/hocs/document/client/documentclient/DocumentClient.java
@@ -7,9 +7,12 @@ import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import uk.gov.digital.ho.hocs.document.application.RequestData;
 import uk.gov.digital.ho.hocs.document.client.documentclient.dto.ProcessDocumentRequest;
 import uk.gov.digital.ho.hocs.document.exception.ApplicationExceptions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static net.logstash.logback.argument.StructuredArguments.value;
@@ -22,23 +25,35 @@ public class DocumentClient {
     private final String documentQueue;
     private final ProducerTemplate producerTemplate;
     private final ObjectMapper objectMapper;
+    private final RequestData requestData;
 
     @Autowired
     public DocumentClient(ProducerTemplate producerTemplate,
                           @Value("${docs.queue}") String documentQueue,
-                          ObjectMapper objectMapper){
+                          ObjectMapper objectMapper,
+                          RequestData requestData){
         this.producerTemplate = producerTemplate;
         this.documentQueue = documentQueue;
         this.objectMapper = objectMapper;
+        this.requestData = requestData;
     }
 
     public void processDocument(UUID documentUUID, String fileLocation, String convertTo) {
         ProcessDocumentRequest request = new ProcessDocumentRequest(documentUUID, fileLocation, convertTo);
         try {
-            producerTemplate.sendBody(documentQueue, objectMapper.writeValueAsString(request));
+            producerTemplate.sendBodyAndHeaders(documentQueue, objectMapper.writeValueAsString(request), getQueueHeaders());
             log.info("Processed Document {}", documentUUID, value(EVENT, DOCUMENT_CLIENT_PROCESS_SUCCESS));
         } catch (JsonProcessingException e) {
             throw new ApplicationExceptions.EntityCreationException(String.format("Could not process Document: %s", e), DOCUMENT_CLIENT_FAILURE);
         }
+    }
+
+    private Map<String, Object> getQueueHeaders() {
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(RequestData.CORRELATION_ID_HEADER, requestData.correlationId());
+        headers.put(RequestData.USER_ID_HEADER, requestData.userId());
+        headers.put(RequestData.USERNAME_HEADER, requestData.username());
+        headers.put(RequestData.GROUP_HEADER, requestData.groups());
+        return headers;
     }
 }
